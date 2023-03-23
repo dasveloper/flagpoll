3eda6029786d40718298c9d01adb9d63c4e1c005
dasveloper <28981975+dasveloper@users.noreply.github.com>
2023-03-23 14:58:58 -0400

    Add codeblock demo

--- src/components/home/Demo.tsx
+++ src/components/home/Demo.tsx
@@ -3,9 +3,15 @@ import { useState } from "react";
 const Demo = () => {
   const [showHeader, setShowHeader] = useState(true);
   const [showHero, setShowHero] = useState(true);
-  const [showCta, setShowCta] = useState(true);
   const [showNewsletter, setShowNewsletter] = useState(true);
   const [showFooter, setShowFooter] = useState(true);
+
+  const objString = `{
+    "header": ${showHeader.toString()},
+    "hero": ${showHero.toString()},
+    "newsletter": ${showNewsletter.toString()},
+    "footer": ${showFooter.toString()},
+  }`;
 
   return (
     <div className="flex flex-col items-center justify-evenly gap-8 md:flex-row">
@@ -23,11 +29,6 @@ const Demo = () => {
                 <span className="text-lg text-white">Hero image</span>
               </div>
             )}
-            {showCta && (
-              <div className="flex w-full flex-1 items-center justify-center bg-amber-500 text-white">
-                <span className="text-lg text-white">Call to action</span>
-              </div>
-            )}
             {showNewsletter && (
               <div className="flex w-full flex-1 items-center justify-center bg-emerald-500 text-white">
                 <span className="text-lg text-white">Newsletter section</span>
@@ -41,53 +42,50 @@ const Demo = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex flex-col items-center justify-center gap-y-8 rounded-xl border border-neutral-300 p-12">
-        <label className="label cursor-pointer gap-x-4">
-          <input
-            type="checkbox"
-            className="toggle-success toggle"
-            checked={showHeader}
-            onChange={() => setShowHeader((prev) => !prev)}
-          />
-          <span className="label-text">Header</span>
-        </label>
-        <label className="label cursor-pointer gap-x-4">
-          <input
-            type="checkbox"
-            className="toggle-success toggle "
-            checked={showHero}
-            onChange={() => setShowHero((prev) => !prev)}
-          />
-          <span className="label-text">Hero image</span>
-        </label>
-        <label className="label cursor-pointer gap-x-4">
-          <input
-            type="checkbox"
-            className="toggle-success toggle "
-            checked={showCta}
-            onChange={() => setShowCta((prev) => !prev)}
-          />
-          <span className="label-text">Call to action</span>
-        </label>
-        <label className="label cursor-pointer gap-x-4">
-          <input
-            type="checkbox"
-            className="toggle-success toggle "
-            checked={showNewsletter}
-            onChange={() => setShowNewsletter((prev) => !prev)}
-          />
-          <span className="label-text">Newsletter section</span>
-        </label>
-        <label className="label cursor-pointer gap-x-4">
-          <input
-            type="checkbox"
-            className="toggle-success toggle "
-            checked={showFooter}
-            onChange={() => setShowFooter((prev) => !prev)}
-          />
-          <span className="label-text">Footer</span>
-        </label>
+      <div className="flex flex-col items-center justify-center gap-y-8">
+        <div className="flex flex-col items-center justify-center gap-y-8 rounded-xl border border-neutral-300 p-12">
+          <label className="label cursor-pointer gap-x-4">
+            <input
+              type="checkbox"
+              className="toggle-success toggle"
+              checked={showHeader}
+              onChange={() => setShowHeader((prev) => !prev)}
+            />
+            <span className="label-text">Header</span>
+          </label>
+          <label className="label cursor-pointer gap-x-4">
+            <input
+              type="checkbox"
+              className="toggle-success toggle "
+              checked={showHero}
+              onChange={() => setShowHero((prev) => !prev)}
+            />
+            <span className="label-text">Hero image</span>
+          </label>
+          <label className="label cursor-pointer gap-x-4">
+            <input
+              type="checkbox"
+              className="toggle-success toggle "
+              checked={showNewsletter}
+              onChange={() => setShowNewsletter((prev) => !prev)}
+            />
+            <span className="label-text">Newsletter section</span>
+          </label>
+          <label className="label cursor-pointer gap-x-4">
+            <input
+              type="checkbox"
+              className="toggle-success toggle "
+              checked={showFooter}
+              onChange={() => setShowFooter((prev) => !prev)}
+            />
+            <span className="label-text">Footer</span>
+          </label>
+        </div>
+        <div className="mockup-code">
+          <pre>
+            <code>{objString}</code>
+          </pre>
+        </div>
       </div>
     </div>
   );
