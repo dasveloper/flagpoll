7ccef8bcf498a06cffc52d3438cd54ae045de8e3
dasveloper <28981975+dasveloper@users.noreply.github.com>
2023-03-23 15:05:34 -0400

    Favicon/seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,17 +7,45 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 import { ModalProvider } from "~/components/dashboard/ModalContext";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <ModalProvider>
-        <Component {...pageProps} />
-      </ModalProvider>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Flagpoll - Feature Flags Made Easy</title>
+        <meta
+          name="description"
+          content="Flagpoll lets you quickly and easily set up feature flags to safely toggle features on and off without redeploying."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <SessionProvider session={session}>
+        <ModalProvider>
+          <Component {...pageProps} />
+        </ModalProvider>
+      </SessionProvider>
+    </>
   );
 };
 
